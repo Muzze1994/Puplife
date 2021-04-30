d06be35a61ac32bcd2cccb387f876a472775cad3
Rasmus Lind <rasmusnyberglind@hotmail.com>
2021-04-30 16:57:02 +0200

    Forum update
    Updated forum with a new view component and create forum post component.

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-
+import CreateForumPost from './Create-forum-post.js';
+import ForumPost from './Forum-post.js';
+import Login from './Login.js';
 
 class Forum extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
     render() {
         return (
             <div>
-                <p>
-                    Forum
-          </p>
+                <Login />
+                <CreateForumPost/>
             </div>
         )
     }
 }
 
 export default Forum
+
+
+
